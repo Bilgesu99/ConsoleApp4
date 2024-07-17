cfd413f5bf78530a1a90989257f4dd2736487ba6
User <User@LAPTOP-64D41OTG>
2024-07-17 11:44:29 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace BilgeSuKuzucular
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bilge Su Kuzucular");
+           Console.WriteLine("Daha önce hiç bilmediğim yazılım hakkında bilgi sahibi olmak ve ortaya iyi ürünler çıkartmayı hedefliyorum");
+        }
+    }
+
+
+
+}
+
+
+
+
+
